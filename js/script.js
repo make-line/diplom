3de3f5de6d4869b67ed27e21d172d651afe44500
make-line <enariiana@gmail.com>
2021-03-27 19:24:19 +0300

    site

--- js/script.js
+++ js/script.js
@@ -787,14 +787,6 @@
 			}
 		}
 
-		// UI To Top
-		if ( isDesktop && !isNoviBuilder ) {
-			$().UItoTop( {
-				easingType:     'easeOutQuad',
-				containerClass: 'ui-to-top fa fa-angle-up'
-			} );
-		}
-
 		// lightGallery
 		if (plugins.lightGallery.length) {
 			for (var i = 0; i < plugins.lightGallery.length; i++) {
